4285179345d9999ee75cef23a9b37757684a370b
Jason Murphy <jason.murphy@analog.com>
2022-03-30 21:46:17 +0100

    Fix indentation

--- adc/adc.asm
+++ adc/adc.asm
@@ -4,18 +4,18 @@
 		list 		P=18F452		
 		include 	<P18F452.inc>	
 
-DlyPar	equ			0x00			; Variable for Tacq delay routine
+DlyPar	        equ		0x00		       ; variable for Tacq delay routine
 
-		org			0x0000			
+		org	        0x0000			
 		goto 		Main
 		
 		org 		0x0008			
 		goto 		ADCisr
 
-		org			0x0028			
+		org		0x0028			
 		
 Main:	
-        clrf		PORTA			; configure i/o ports
+                clrf		PORTA			; configure i/o ports
 		setf		TRISA							
 		
 		clrf		PORTB
@@ -27,36 +27,35 @@ Main:
 		movlw		0x4E			
 		movwf		ADCON1			
 
-		bsf			PIR1,ADIF		; enable the A/D interrupt
-		bsf			INTCON,PEIE		; enable peripheral interrupts
-		bsf			INTCON,GIE		; enable global interrupts
+		bsf		PIR1,ADIF		; enable the A/D interrupt
+		bsf		INTCON,PEIE		; enable peripheral interrupts
+		bsf		INTCON,GIE		; enable global interrupts
 
-		bsf			ADCON0,GO		; start first A/D conversion process
+		bsf		ADCON0,GO		; start first A/D conversion process
 				
-Idle:	bra			Idle			; repeat indefinitely
+Idle:	        bra		Idle			; repeat indefinitely
 
 ;------------------------------------------------------------------------------------------
 
 ADCisr:	
-        btfss		PIR1,ADIF		; is interrupt from A/D unit?
-		bra			XitIsr			; no, false interrupt, abort
+                btfss		PIR1,ADIF		; is interrupt from A/D unit?
+		bra		XitIsr			; no, false interrupt, abort
 		movf		ADRESH,W		; yes, get MSW of A/D result
 		movwf		PORTB			; display 4 LSBs on LEDs
 		call 		Tacq			; yes, wait acquisition time period
-		bcf			PIR1,ADIF		; reset A/D intrpt flag
-		bsf			ADCON0,GO		; begin a new A/D conversion process
-XitIsr:	
-        retfie						; return to main prog
+		bcf		PIR1,ADIF		; reset A/D intrpt flag
+		bsf		ADCON0,GO		; begin a new A/D conversion process
+
+XitIsr:	        retfie					; return to main prog
 
 ;------------------------------------------------------------------------------------------
 
-Tacq:	
-        movlw		0x04			; set delay to allow for
+Tacq:	        movlw		0x04			; set delay to allow for
 		movwf		DlyPar			; proper signal acquisition
 Delay:	
-        decfsz		DlyPar,F		; by allowing C_hold to 
-		bra			Delay			; charge up fully before A/D conversion
-		return						; return to caller	
+                decfsz		DlyPar,F		; by allowing C_hold to 
+		bra		Delay			; charge up fully before A/D conversion
+		return					; return to caller	
 			
 ;==========================================================================================
-		end
+		END
--- io/led-count.asm
+++ io/led-count.asm
@@ -1,61 +1,60 @@
 ; Program that counts to 8 on LEDs connected to PORTB each time a switch on PORTA is pressed   
             
-            list 	P=18F452		
-            include <p18f452.inc>	
+            	list 		P=18F452		
+            	include 	<p18f452.inc>	
 
 DelayCount1	equ		0x00			; Extra variables for use in the program
 DelayCount2	equ		0x01			; See memory map on Data Sheet, page 16
 DisplayVal	equ		0x02
 
-            org		0x0000			; Reset/Start Vector of the microcontroller
-            goto	Start
+            	org		0x0000			; Reset/Start Vector of the microcontroller
+            	goto		Start
             
-            org		0x0028			; Place the assembly code in memory here
+            	org		0x0028			; Place the assembly code in memory here
             
 Start:		
-            clrf	PORTA			; Clear PORTA file register
-            clrf	PORTB			; Clear PORTB file register
+            	clrf		PORTA			; Clear PORTA file register
+            	clrf		PORTB			; Clear PORTB file register
 
-            movlw	0xFF			; Set PORTA to be all inputs
-            movwf 	TRISA			
-            movlw	0x00			; Set PORTB RB0:RB7 as outputs for LEDs
-            movwf	TRISB			
+            	movlw		0xFF			; Set PORTA to be all inputs
+            	movwf 		TRISA			
+            	movlw		0x00			; Set PORTB RB0:RB7 as outputs for LEDs
+            	movwf		TRISB			
     
 WaitStart:	
-            btfsc	PORTA,4			; S2 pressed? 
-            goto 	WaitStart		; No, wait and check again
+            	btfsc		PORTA,4			; S2 pressed? 
+            	goto 		WaitStart		; No, wait and check again
             
-            movlw	0x00			; Yes, reset the count
-            movwf	DisplayVal		
+            	movlw		0x00			; Yes, reset the count
+            	movwf		DisplayVal		
 
 CountLoop:	
-            movf	DisplayVal,W	; Get the current display value
-            movwf	PORTB			; Output value on LEDs
+            	movf		DisplayVal,W		; Get the current display value
+            	movwf		PORTB			; Output value on LEDs
 
-            call 	SoftDelay		; Delay to slow down the PIC
+             	call 		SoftDelay		; Delay to slow down the PIC
             
-            incf	DisplayVal,F	; Increment the display value
-            movf	DisplayVal,W	; Get the new value
-            sublw	0x10			; Has it reached its max value?
-            bnz		CountLoop		; No, continue incrementing			
-            bra		WaitStart		; Yes, wait for S2 to start again		
-
+            	incf		DisplayVal,F		; Increment the display value
+            	movf		DisplayVal,W		; Get the new value
+            	sublw		0x10			; Has it reached its max value?
+            	bnz		CountLoop		; No, continue incrementing			
+            	bra		WaitStart		; Yes, wait for S2 to start again		
 
 
 SoftDelay:	
-            clrf	DelayCount1		; Implements nested loops to slow down PIC
-            movlw	0xC0
-            movwf	DelayCount2
+            	clrf		DelayCount1		; Implements nested loops to slow down PIC
+            	movlw		0xC0
+            	movwf		DelayCount2
 
 DelayLoop:	
-            decf	DelayCount1,F	; Outer Loop of Delay Loop
-            bnz		SmallDelay
-            bra 	ExitDelay
+           	decf		DelayCount1,F		; Outer Loop of Delay Loop
+            	bnz		SmallDelay
+            	bra 		ExitDelay
 
 SmallDelay:	
-            decf	DelayCount2,F	; Inner Loop of Delay Loop
-            bnz		SmallDelay
-            bra 	DelayLoop
+            	decf		DelayCount2,F		; Inner Loop of Delay Loop
+            	bnz		SmallDelay
+            	bra 		DelayLoop
 
 ExitDelay:	return
 
